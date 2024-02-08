d6249ee4a11cc1ad5210104b589465d4060470d7
Knellec <ethancknell@gmail.com>
2024-02-07 21:56:54 -0500

    Lab 4 Task 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
-public class Main {
+public class Main
+{
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // declare variables
+        double summerCost = 183.66;
+        double autumnCost = 166.50;
+        double winterCost = 170.32;
+        double springCost = 161.12;
+        double yearlyCost = 0;
+
+        // calculate total yearly maintenance cost
+        yearlyCost = summerCost + autumnCost + winterCost + springCost;
+
+        // output
+        System.out.println("The maintenance cost for the summer is " + summerCost);
+        System.out.println("The maintenance cost for the autumn is " + autumnCost);
+        System.out.println("The maintenance cost for the winter is " + winterCost);
+        System.out.println("The maintenance cost for the spring is " + springCost);
+
+        System.out.println("The total yearly maintenance cost is " + yearlyCost);
     }
 }
